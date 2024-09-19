3fbdb158854d95bfc5358b3c54eae04f9b9f6351
FlaxidHair <mrkmarlou@gmail.com>
2024-09-19 18:56:46 +0300

    feat: edit type input for FORK TO FROM  on number without arrow up/down

--- src/views/App-EditItem.vue
+++ src/views/App-EditItem.vue
@@ -2,59 +2,59 @@
     <div>
         <div class="add-item">
     <div class="add-item--inner">
-      <h2 class="title add-item__title">Новое собеседование</h2>
+      <h2 class="title add-item__title">Собеседование в компанию {{ nameStage }}</h2>
       <div class="add-item__inputs inputs">
         <input
           type="text"
           placeholder="Компания"
           class="inputs__item inputs--company"
-          v-model="company"
+          v-model="company" id="company"
         />
         <input
           type="text"
           placeholder="Описание вакансии(ссылка)"
           class="inputs__item inputs--desc"
-          v-model="description"
+          v-model="description" id="description"
         />
         <input
           type="text"
           placeholder="Контакт(имя)"
           class="inputs__item inputs--contact"
-          v-model="contactName"
+          v-model="contactName" id="contactName"
         />
         <input
           type="text"
           placeholder="Telegram username HR"
           class="inputs__item inputs--contact-telegram"
-          v-model="telegram"
+          v-model="telegram" id="telegram"
         />
         <input
           type="text"
           placeholder="Viber телефон HR"
           class="inputs__item inputs--contact-viber"
-          v-model="viber"
+          v-model="viber" id="viber"
         />
         <input
           type="text"
           placeholder="Телефон HR"
           class="inputs__item inputs--contact-phone"
-          v-model="phone"
+          v-model="phone" id="phone"
         />
       </div>
       <div class="inputs__more">
         <div class="inputs__more-fork">
-            <input class="inputs__more-fork--from inputs-fork" placeholder="Зарплатная вилка ОТ" type="text">
-            <input class="inputs__more-fork--to inputs-fork" placeholder="Зарплатная вилка ДО" type="text">
+            <input class="inputs__more-fork--from inputs-fork" id="forkFrom" placeholder="Зарплатная вилка ОТ" type="number">
+            <input class="inputs__more-fork--to inputs-fork" id="forkTo" placeholder="Зарплатная вилка ДО" type="number">
         </div>
         <div class="inputs__more-stages">
             <button class="inputs__more-stages-button">
-                <SvgIcon type="mdi" :path="mdiPlus" size="24"/> Добавить этап
+                <SvgIcon type="mdi" :path="mdiPlus" size="24"/> Добавить этапnumber
             </button>
             <div class="inputs__more-stages-card stages-card">
                 <label class="label" for="name-stage">Название этапа</label>
-                <input type="text" class="inputs__more-stages-card-input input--name" id="name-stage" title="Название этапа собеседования" placeholder="Название этапа">
+                <input type="text" id="nameStage" class="inputs__more-stages-card-input input--name" title="Название этапа собеседования" placeholder="Название этапа">
                 <label class="label" for="date-stage" >Дата прохождения этапа</label>
-                <input type="date" class="inputs__more-stages-card-input input--date" id="date-stage" title="Дата прохождения собеседования">
+                <input type="date" id="dateStage" class="inputs__more-stages-card-input input--date" title="Дата прохождения собеседования">
                 <label class="label" for="comm-stage">Комментарий</label>
                 <textarea id="comm-stage"  class="stages-card__comment" placeholder="Комментарий к этапу"></textarea>
                 <button class="stages-card__delete-button">Удалить этап</button>
@@ -91,6 +91,11 @@ const telegram = ref<string>('')
 const viber = ref<string>('')
 const phone = ref<string>('')
 
+const forkfrom = ref<string>('')
+const forkTo = ref<string>('')
+const nameStage = ref<string>('')
+const dateStage = ref<string>('')
+const commentStage = ref<string>('')
 
 </script>
 
