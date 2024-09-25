b86898680047f21916da4202b89688c04ba8dee5
FlaxidHair <mrkmarlou@gmail.com>
2024-09-25 14:03:30 +0300

    refactor:  delete component pie chart

--- src/views/App-Stat.vue
+++ src/views/App-Stat.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="stat">
-<BarChart class="stat__table"></BarChart>
+    <h2 class="stat__title">Статистика собеседований</h2>
+    <Doughnut class="stat__table" :data="chartData"></Doughnut>
   </div>
 </template>
 
-<script setup lang="ts">
-import BarChart from '../components/AppPieChart.vue'
+<script lang="ts" setup>
+import {Chart as chartJS,Tooltip,Legend,ArcElement} from 'chart.js'
+import {Doughnut} from 'vue-chartjs'
+
+chartJS.register(
+  Tooltip,Legend,ArcElement
+)
+ 
+ const chartData = {
+  labels:[
+    'Оффер',
+    'Отказ',
+    'В ожидании',
+  ],
+  datasets:[
+    {
+      label:'Собеседований',
+      backgroundColor:["#d44950","#59a969",'#4f5a6e'],
+      data:[10,20,5],
+    },
+  ]
+ }
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/scss/blocks/PieChart';
 </style>
